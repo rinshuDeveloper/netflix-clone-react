5436aaec1ebc7b35d88eacfb417c4c14a9e3a7bd
Rinshad <rinshufreelancer@gmail.com>
2026-06-16 01:22:23 +0530

    Added movie trailers and updated posters

--- src/Components/Movies/Movies.jsx
+++ src/Components/Movies/Movies.jsx
@@ -1,26 +1,45 @@
 import React, { useEffect, useState } from "react";
+import YouTube from "react-youtube";
 import "./Movies.css";
 import axios from "../../axios";
 
 function Movies(props) {
   const [movies, setMovies] = useState([]);
   const [actions, setActions] = useState([]);
+  const [urlId, setUrlId] = useState("");
 
-    useEffect(() => {
-
-      if(props.isSmall) {
-        axios.get(axios.actions).then((response) => {
+  useEffect(() => {
+    if (props.isSmall) {
+      axios.get(axios.actions).then((response) => {
         setMovies(response.config.actions.movies);
       });
-      } else {
-        axios.get(axios.movies).then((response) => {
+    } else {
+      axios.get(axios.movies).then((response) => {
         setMovies(response.config.movies);
       });
-      }
-      
-    }, []);
-  
+    }
+  }, []);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+   
+  const handleTrailer = (id) => {
+      const movie = movies.find((item) => item.id === id);
+      if (movie?.key) {
+        setUrlId(movie)
+        console.log(movie);
+      }else {
+        console.log("Trailer is not available!!");
+        
+      }
+    
+  };
   return (
     <div className="row">
       <h2 className="row-heading">{props.title}</h2>
@@ -28,7 +47,8 @@ function Movies(props) {
         {movies.map((obj, index) => {
           return (
             <img
-            key={index}
+              onClick={() => handleTrailer(obj.id)}
+              key={index}
               src={`${obj.image}`}
               alt=""
               className={props.isSmall ? "actionMovies" : "movies"}
@@ -36,6 +56,12 @@ function Movies(props) {
           );
         })}
       </div>
+
+      
+        { urlId && (<div className="yt-container">
+          <YouTube videoId={urlId.key} opts={opts} />
+        </div>)}
+      
     </div>
   );
 }
